ee95522d18ca05fa02acbc255c2ab0d513cf5dbb
victor <victorgomesdacosta18@gmail.com>
2024-04-26 11:17:08 -0300

    chore: building

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+    const users = await prisma.user.findMany()
+
+    return users
+})
 
 app.listen({
     port: 3333,
-}).then()
+}).then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+})
